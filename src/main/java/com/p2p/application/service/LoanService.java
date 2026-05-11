df29029c79d3b562595c9ec0972fc56e0a388717
Imam Miftahul U'lum <imammiftahululum200@gmail.com>
2026-05-11 22:33:46 +0700

    Create LoanService.java

---
+++ src/main/java/com/p2p/application/service/LoanService.java
@@ -0,0 +1,16 @@
+package com.p2p.application.service;
+
+import com.p2p.domain.loan.Loan;
+import com.p2p.domain.loan.LoanRepository;
+import com.p2p.domain.valueobject.Money;
+
+public class LoanService {
+    
+    public LoanService(LoanRepository loanRepository) {
+
+    }
+
+    public void bayarCicilan(String loanId, Money amount) {
+    
+    }
+}

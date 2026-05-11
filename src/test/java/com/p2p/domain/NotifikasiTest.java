b05b6d6a7af614736d9e5af899bca6bd4e71a31d
Rajbi Nizar <rajbinizar28@gmail.com>
2026-05-11 23:31:48 +0700

    test(domain): RED - NotifikasiTest loan_berstatus_DISBURSED_layak_notifikasi_berhasil

---
+++ src/test/java/com/p2p/domain/NotifikasiTest.java
@@ -0,0 +1,19 @@
+package com.p2p.domain;
+
+import com.p2p.domain.loan.Loan;
+import com.p2p.domain.valueobject.Money;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotifikasiTest {
+
+    @Test
+    void loan_berstatus_DISBURSED_layak_notifikasi_berhasil() {
+        Loan loan = new Loan("L-001", "BR-001", new Money(new BigDecimal("10000000"), "IDR"));
+        loan.ubahStatus("DISBURSED");
+        assertTrue(loan.isLayakNotifikasiPencairan());
+    }
+}

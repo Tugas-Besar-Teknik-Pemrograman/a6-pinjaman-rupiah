25146ef75fc3204365911ffb64f37b71b9e1259e
Imam Miftahul U'lum <imammiftahululum200@gmail.com>
2026-05-11 22:33:30 +0700

    Update Loan.java

--- src/main/java/com/p2p/domain/loan/Loan.java
+++ src/main/java/com/p2p/domain/loan/Loan.java
@@ -1,45 +1,45 @@
 package com.p2p.domain.loan;
+
 import com.p2p.domain.valueobject.Money;
-import java.util.ArrayList;
-import java.util.List;
+import com.p2p.domain.loan.strategy.InterestCalculationStrategy;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Loan {
-    // --- ATRIBUT DASAR ---
     private String id;
     private String borrowerId;
     private Money targetNominal;
-    private Money totalTerkumpul;
-
+    private Money remainingPrincipal;
+    private int tenor;
     private String status;
+    
+    private InterestCalculationStrategy interestStrategy;
+    private Money currentMonthBill;
 
-    // private List<Repayment> daftarCicilan = new ArrayList<>();
+    public Loan(String id, String borrowerId, Money targetNominal, int tenor) {
 
-    // --- CONSTRUCTOR ---
-    public Loan(String id, String borrowerId, Money targetNominal) {
-        this.id = id;
-        this.borrowerId = borrowerId;
-        this.targetNominal = targetNominal;
-        this.totalTerkumpul = new Money(new java.math.BigDecimal("0"), "IDR");
-        this.status = "PROPOSED";
     }
 
-    // --- METHOD KERANGKA UNTUK TDD MASING-MASING ---
+    public void setInterestStrategy(InterestCalculationStrategy strategy) {
 
-    public void ubahStatus(String statusBaru) {
-        // Jangan diisi dulu, biarkan yang isi saat ngerjain TDD-nya
     }
 
-    public void tambahPendanaan(String lenderId, Money investasiDiberikan) {
-        // Tambahkan nilai ke totalTerkumpul
-        // Bikin if-condition, kalau totalTerkumpul == targetNominal, trigger Observer (Event)
+    public void generateMonthlyBill() {
+
     }
 
-    public void bayarCicilan(String repaymentId, Money jumlahBayar) {
-        //  yang akan isi logikanya nanti
+    public void payInstallment(Money paymentAmount) {
+
+        }
+
+    public void ubahStatus(String status) {
+
     }
 
-    // Getter...
-    public String getId() { return id; }
-    public String getStatus() { return status; }
+    public String getId() {
+        return id;
+    }
 
+    public Money getCurrentMonthBill() { return currentMonthBill; }
+    
 }

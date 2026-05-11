45b75e131d4f4ce0b3665142a1cc7e5f135310ca
Darva Aryasatya Putra Hermawan <darva.aryasatya.tif425@polban.ac.id>
2026-05-11 20:27:48 +0700

    feat: menambahkan hasil template dari cucumber class InvestasiLenderSteps.java

---
+++ src/test/java/com/p2p/domain/InvestasiLenderSteps.java
@@ -0,0 +1,56 @@
+package com.p2p.domain;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class InvestasiLenderSteps {
+    
+    // Givern
+    @Given("Loan dengan status FUNDING")
+    public void loan_dengan_status_funding() {
+        
+    }
+
+    @Given("Loan dengan status not FUNDING")
+    public void loan_dengan_status_not_funding() {
+        
+    }
+    
+    // When
+    @When("Lender input dana yang ingin diberikan")
+    public void lender_input_dana_yang_ingin_diberikan() {
+        
+    }
+
+    @When("Lender input dana investasi <= {int}")
+    public void lender_input_dana_investasi(Integer int1) {
+        
+    }
+
+    @When("Lender input dana yang ingin diberikan > target")
+    public void lender_input_dana_yang_ingin_diberikan_target() {
+
+    }
+    
+    // Then
+    @Then("Loan akan akan terisi sesuai nominal dana yang di input")
+    public void loan_akan_akan_terisi_sesuai_nominal_dana_yang_di_input() {
+        
+    }
+    
+    @Then("Sistem akan menolak dengan pesan error karena status not FUNDING")
+    public void sistem_akan_menolak_dengan_pesan_error_karena_status_not_funding() {
+        
+    }
+    
+    @Then("Sistem harus menolak pengajuan dengan pesan error")
+    public void sistem_harus_menolak_pengajuan_dengan_pesan_error() {
+        
+    }
+    
+    @Then("Sistem akan menolak dengan pesan error")
+    public void sistem_akan_menolak_dengan_pesan_error() {
+
+    }
+}
--- src/test/resources/features/InvestasiLender.feature
+++ src/test/resources/features/InvestasiLender.feature
@@ -8,10 +8,10 @@ Feature: Pengajuan Investasi Lender
     When Lender input dana yang ingin diberikan
     Then Loan akan akan terisi sesuai nominal dana yang di input
 
-  Scenario: [IL-02] Pengajuan investasi berhasil dengan pada status loan !FUNDING
-    Given Loan dengan status !FUNDING
+  Scenario: [IL-02] Pengajuan investasi berhasil dengan pada status loan not FUNDING
+    Given Loan dengan status not FUNDING
     When Lender input dana yang ingin diberikan
-    Then Sistem akan menolak dengan pesan error karena status !FUNDING
+    Then Sistem akan menolak dengan pesan error karena status not FUNDING
 
   Scenario: [IL-03] Menolak investasi jika nominal nol atau negatif
     Given Loan dengan status FUNDING

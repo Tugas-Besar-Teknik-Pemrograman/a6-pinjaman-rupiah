630af5594c8d974a095d765df988b152b59c6c0a
Muhammad Faqih Shiam <faqihshiam23@gmail.com>
2026-05-11 23:08:49 +0700

    feat: clean bug steps definition cucumber

--- src/main/java/com/p2p/domain/borrower/BorrowerRepository.java
+++ src/main/java/com/p2p/domain/borrower/BorrowerRepository.java
@@ -1,7 +1,5 @@
 package com.p2p.domain.borrower;
 
-import java.util.Optional;
-
 public interface BorrowerRepository {
-    Optional<Borrower> findById(String id);
+    Borrower findById(String id);
 }

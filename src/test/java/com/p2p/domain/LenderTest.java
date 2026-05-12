62a33af52263920321d962ae84f63b58c47f4eab
Darva Aryasatya Putra Hermawan <darva.aryasatya.tif425@polban.ac.id>
2026-05-12 10:57:25 +0700

    feat: implement tambahSaldo method and corresponding unit tests in Lender class

--- src/test/java/com/p2p/domain/LenderTest.java
+++ src/test/java/com/p2p/domain/LenderTest.java
@@ -1,4 +1,57 @@
 package com.p2p.domain;
 
+import com.p2p.domain.lender.Lender;
+import com.p2p.domain.valueobject.Money;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LenderTest {
+    private Lender lender;
+
+    @BeforeEach
+    void  inisialisasi() {
+        lender = new Lender("LDR-001", new Money(new BigDecimal("5000000"), "IDR"));
+        lender.setKycStatus(true);
+    }
+
+    @Test
+    void tarikSaldo_Sukses_SaldoTerkurang() {
+        // Arrange
+        Money nominalTarik = new Money(new BigDecimal("500000"), "IDR");
+        BigDecimal saldoAwalExpected = new BigDecimal("5000000");
+        BigDecimal sisaSaldoExpected = new BigDecimal("4500000");
+
+        // Assert saldo awal
+        assertEquals(saldoAwalExpected, lender.getSaldoBalance().getAmount());
+
+        // Act
+        assertDoesNotThrow(() -> lender.tarikSaldo(nominalTarik));
+
+        // Assert saldo berkurang
+        assertEquals(sisaSaldoExpected, lender.getSaldoBalance().getAmount());
+    }
+
+    @Test
+    void tambahSaldo_Sukses_SaldoBertambah() {
+        // Arrange
+        Money nominalTambah = new Money(new BigDecimal("2000000"), "IDR");
+        BigDecimal saldoAwalExpected = new BigDecimal("5000000");
+        BigDecimal saldoBaruExpected = new BigDecimal("7000000");
+
+        // Assert saldo awal
+        assertEquals(saldoAwalExpected, lender.getSaldoBalance().getAmount());
+
+        // Act
+        assertDoesNotThrow(() -> lender.tambahSaldo(nominalTambah));
+
+        // Assert saldo bertambah
+        assertEquals(saldoBaruExpected, lender.getSaldoBalance().getAmount());
+    }
+
+    
+
 }

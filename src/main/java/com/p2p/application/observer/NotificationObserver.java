3d1aa75e06adefa9c9f1b5e07390f89027c46f77
Rajbi Nizar <rajbinizar28@gmail.com>
2026-05-12 16:42:08 +0700

    feat(application): add NotificationObserver untuk handle event pencairan

---
+++ src/main/java/com/p2p/application/observer/NotificationObserver.java
@@ -0,0 +1,15 @@
+package com.p2p.application.observer;
+
+import com.p2p.domain.event.PencairanBerhasilEvent;
+
+public class NotificationObserver {
+    public void onPencairanBerhasil(PencairanBerhasilEvent event) {
+        String borrowerId = event.getBorrowerId();
+        String loanId = event.getLoanId();
+        System.out.println("Notifikasi ke Borrower" + borrowerId + ": Dana pinjaman" + loanId + " telah berhasil dicairkan!");
+    }
+
+    publiic void onPencairanGagal(String borrowerId, String loanId, String reason) {
+        System.out.println("Notifikasi ke Borrower " + borrowerId + ": Pencairan dana untuk pinjaman " + loanId + " gagal. Alasan: " + reason);
+    }
+}

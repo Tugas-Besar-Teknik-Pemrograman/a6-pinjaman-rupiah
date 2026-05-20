110ef36b6e634b7f7b1657d62c3a923759074a48
unknown <arsel.fahri.tif425@polban.ac.id>
2026-05-20 16:08:09 +0700

    fix: add more money to SSP test9

--- src/test/java/com/p2p/domain/SiklusStatusTest.java
+++ src/test/java/com/p2p/domain/SiklusStatusTest.java
@@ -160,7 +160,7 @@ public class SiklusStatusTest {
  
         // Generate tagihan dan bayar
         loan.generateMonthlyBill();
-        Money paymentAmount = new Money(new BigDecimal("13334"), "IDR");
+        Money paymentAmount = new Money(new BigDecimal("63334"), "IDR");
         loan.payInstallment(paymentAmount);
  
         // Setelah bayar, currentMonthBill = 0 → isOverduePaid() = true
